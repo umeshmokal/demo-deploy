278b3479185136e46932ee60204baf027ed09b27
umeshmokal <umeshmokal08@gmail.com>
2021-04-16 19:41:31 +0530

    updates for deploy app on heroku

--- demo-deploy/server.js
+++ demo-deploy/server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const path = require('path');
+import express, { static } from 'express';
+import { join } from 'path';
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/demo-deploy'));
+app.use(static(__dirname + '/dist/demo-deploy'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
+res.sendFile(join(__dirname+'/dist/demo-deploy/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
